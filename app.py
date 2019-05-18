ef46d4846056724eb3b79fa569812977a2b97def
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-05-18 16:54:07 +0800

    Update app.py
    Add OpenCV Live Streaming

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, Response
 
 # Raspberry Pi camera module (requires picamera package)
 from video import Camera
+from videoCV import VideoCamera
 from picamera import PiCamera
 import time
 
 app = Flask(__name__)
 
-
+# home page
 @app.route("/")
 def index():
 	"""Main page."""	
 	return render_template('index.html')
 
+# take picture page
 @app.route('/photo')
 def photo():
 	""" take a photo """
@@ -31,7 +33,7 @@ def CameraPic():
 	return 'http://1.1.1.34:5000'+ filename
 
 	
-
+#video streamging use import picamera
 @app.route('/camera')
 def cam():
 	"""Video streaming home page."""
@@ -54,6 +56,27 @@ def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# video streaming use OpenCV
+@app.route('/camera-OpenCV')
+def camCV():
+	return render_template('camera-OpenCV.html')
+
+def gen(videoCV):
+    """Video streaming generator function."""
+    while True:
+        frame = videoCV.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feedCV')
+def video_feedCV():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='1.1.1.34', port=5050, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=6006, debug=True, threaded=True)
